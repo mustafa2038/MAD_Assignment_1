6778549dc279cf83c0010dd56874eac016c5e294
Mustafa <20904894@student.curtin.edu.au>
2023-09-06 15:57:59 +0800

    added player 1 and player 2 to GameFragment

--- TicTacToe/app/src/main/java/com/example/tictactoe/GameFragment.java
+++ TicTacToe/app/src/main/java/com/example/tictactoe/GameFragment.java
@@ -9,11 +9,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class GameFragment extends Fragment {
+    Player player1;
+    Player player2;
     public GameFragment() {
         // Required empty public constructor
     }
@@ -23,19 +26,23 @@ public class GameFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_game, container, false);
+
         MainActivityData mainActivityDataViewModel = new ViewModelProvider(getActivity()).get(MainActivityData.class);
 
+        player1 = new Player(mainActivityDataViewModel.getPlayer1Name(), mainActivityDataViewModel.getPlayer1AvatarDrawable(),
+                mainActivityDataViewModel.getPlayer1MarkerDrawable());
+        player2 = new Player(mainActivityDataViewModel.getPlayer1Name(), mainActivityDataViewModel.getPlayer2AvatarDrawable(),
+                mainActivityDataViewModel.getPlayer2MarkerDrawable());
 
-        Button button0 = view.findViewById(R.id.button0);
-        Button button1 = view.findViewById(R.id.button1);
-        Button button2 = view.findViewById(R.id.button2);
-        Button button3 = view.findViewById(R.id.button3);
-        Button button4 = view.findViewById(R.id.button4);
-        Button button5 = view.findViewById(R.id.button5);
-        Button button6 = view.findViewById(R.id.button6);
-        Button button7 = view.findViewById(R.id.button7);
-        Button button8 = view.findViewById(R.id.button8);
-        Button buttonSettings = view.findViewById(R.id.settingButton);
+        ImageButton button0 = view.findViewById(R.id.button0);
+        ImageButton button1 = view.findViewById(R.id.button1);
+        ImageButton button2 = view.findViewById(R.id.button2);
+        ImageButton button3 = view.findViewById(R.id.button3);
+        ImageButton button4 = view.findViewById(R.id.button4);
+        ImageButton button5 = view.findViewById(R.id.button5);
+        ImageButton button6 = view.findViewById(R.id.button6);
+        ImageButton button7 = view.findViewById(R.id.button7);
+        ImageButton button8 = view.findViewById(R.id.button8);
 
         TextView playerIndicatorTextView = view.findViewById(R.id.playerIndicatorTextView);
         TextView timerTextView = view.findViewById(R.id.timerTextView);
@@ -47,7 +54,7 @@ public class GameFragment extends Fragment {
         Button settingButton = view.findViewById(R.id.settingButton);
         Button restartButton = view.findViewById(R.id.restartButton);
 
-        buttonSettings.setOnClickListener(new View.OnClickListener() {
+        settingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mainActivityDataViewModel.setClickedValue("loadInGameSettingsFragment()");
