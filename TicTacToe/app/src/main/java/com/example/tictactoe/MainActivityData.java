dfe337d84b4472588e4f5e86589db58f5ff5c1dc
hanyo2 <hanjien101@gmail.com>
2023-09-06 14:24:49 +0800

    Added saving functionality for gameSettings

--- TicTacToe/app/src/main/java/com/example/tictactoe/MainActivityData.java
+++ TicTacToe/app/src/main/java/com/example/tictactoe/MainActivityData.java
@@ -5,6 +5,8 @@ import androidx.lifecycle.ViewModel;
 
 public class MainActivityData extends ViewModel {
     public MutableLiveData<String> clickedValue = new MutableLiveData<>();
+    public MutableLiveData<String> boardSize = new MutableLiveData<>();
+    public MutableLiveData<String> streak = new MutableLiveData<>();
 
     public MainActivityData() {
         clickedValue.setValue("");
@@ -17,4 +19,21 @@ public class MainActivityData extends ViewModel {
     public void setClickedValue(String value) {
         clickedValue.setValue(value);
     }
+
+    public void setBoardSize(String value){
+        boardSize.setValue(value);
+    }
+
+    public String getBoardSize(){
+        return boardSize.getValue();
+    }
+
+    public void setStreak(String value){
+        streak.setValue(value);
+    }
+
+    public String getStreak(){
+        return streak.getValue();
+    }
+
 }
